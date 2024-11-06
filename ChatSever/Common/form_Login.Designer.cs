f40cc0c2e2254fb58ff3abd2442f6ec053892eee
ThanhHiep <nhiep3445@gmail.com>
2024-11-06 20:19:37 +0700

    Tao Client va form design dang nhap

---
+++ ChatSever/Common/form_Login.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ChatSever.Common
+{
+    partial class form_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_UserName = new System.Windows.Forms.Label();
+            this.lb_Password = new System.Windows.Forms.Label();
+            this.txt_UserName = new System.Windows.Forms.TextBox();
+            this.txt_PassWord = new System.Windows.Forms.TextBox();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb_UserName
+            // 
+            this.lb_UserName.AutoSize = true;
+            this.lb_UserName.Location = new System.Drawing.Point(56, 32);
+            this.lb_UserName.Name = "lb_UserName";
+            this.lb_UserName.Size = new System.Drawing.Size(60, 13);
+            this.lb_UserName.TabIndex = 0;
+            this.lb_UserName.Text = "User Name";
+            // 
+            // lb_Password
+            // 
+            this.lb_Password.AutoSize = true;
+            this.lb_Password.Location = new System.Drawing.Point(56, 63);
+            this.lb_Password.Name = "lb_Password";
+            this.lb_Password.Size = new System.Drawing.Size(56, 13);
+            this.lb_Password.TabIndex = 0;
+            this.lb_Password.Text = "PassWord";
+            // 
+            // txt_UserName
+            // 
+            this.txt_UserName.Location = new System.Drawing.Point(130, 32);
+            this.txt_UserName.Name = "txt_UserName";
+            this.txt_UserName.Size = new System.Drawing.Size(138, 20);
+            this.txt_UserName.TabIndex = 1;
+            // 
+            // txt_PassWord
+            // 
+            this.txt_PassWord.Location = new System.Drawing.Point(130, 63);
+            this.txt_PassWord.Name = "txt_PassWord";
+            this.txt_PassWord.Size = new System.Drawing.Size(138, 20);
+            this.txt_PassWord.TabIndex = 2;
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(70, 114);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(75, 23);
+            this.btn_Login.TabIndex = 3;
+            this.btn_Login.Text = "Login";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Location = new System.Drawing.Point(183, 114);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_Cancel.TabIndex = 4;
+            this.btn_Cancel.Text = "Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // form_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 177);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.txt_PassWord);
+            this.Controls.Add(this.txt_UserName);
+            this.Controls.Add(this.lb_Password);
+            this.Controls.Add(this.lb_UserName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "form_Login";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_UserName;
+        private System.Windows.Forms.Label lb_Password;
+        private System.Windows.Forms.TextBox txt_UserName;
+        private System.Windows.Forms.TextBox txt_PassWord;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Button btn_Cancel;
+    }
+}
---
+++ ChatSever/Common/form_Login.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatSever.Common
+{
+    public partial class form_Login : Form
+    {
+        public form_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = System.Windows.Forms.DialogResult.Cancel;           
+            Close();
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_UserName.Text))
+            {
+                MessageBox.Show(@"User Name không được để trống ", @"System Message");
+                return;
+            }
+            if (string.IsNullOrEmpty(txt_PassWord.Text))
+            {
+                MessageBox.Show(@"PassWord không được để trống ", @"System Message");
+                return;
+            }
+            if ( txt_UserName.Text.Equals("admin") && (txt_PassWord.Text.Equals("123456")))
+            {
+                DialogResult = DialogResult.OK;
+                Close();
+                return;
+            }
+            MessageBox.Show(@"Bạn đã nhập sai User hoặc PassWord, vui lòng nhập lại", @"System Message");
+        }
+    }
+}
