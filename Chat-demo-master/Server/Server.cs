50dd4e22a14bdddce64d385d115be292abb90924
NguyenThanhYYY <2251120199@ut.edu.vn>
2024-11-08 00:16:44 +0700

    Server_GetIpAdress

--- Chat-demo-master/Server/Server.cs
+++ Chat-demo-master/Server/Server.cs
@@ -14,10 +14,47 @@ namespace Server
 {
     public partial class Server : Form
     {
-      
+        private static ConcurrentBag<ClientInfo> clients = new ConcurrentBag<ClientInfo>();
+        private TcpListener server;
+        private byte[] message;
+
         public Server()
         {
             InitializeComponent();
         }
+        //Lấy ip address từ wifi
+        private void ShowWifiIPAddress()
+        {
+            StringBuilder wifiIP = new StringBuilder();
 
+            foreach (NetworkInterface netInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                // Kiểm tra nếu giao diện là Wi-Fi và đang kết nối
+                if (netInterface.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 &&
+                    netInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (UnicastIPAddressInformation ip in netInterface.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork) // IPv4
+                        {
+                            wifiIP.AppendLine(ip.Address.ToString());
+                        }
+                    }
+                }
+            }
+
+            if (txt_IPAddress.InvokeRequired)
+            {
+                txt_IPAddress.Invoke(new Action(() =>
+                    txt_IPAddress.Text = wifiIP.ToString()));
+            }
+            else
+            {
+                txt_IPAddress.Text = wifiIP.ToString();
+            }
+        }
+
+
+
+    }
 }
