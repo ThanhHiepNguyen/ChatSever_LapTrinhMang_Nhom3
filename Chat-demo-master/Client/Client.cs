804af618eaebf59ff2b2a50a10d9e77780196c3b
NguyenHuuPhu112 <phun8218@gmail.com>
2024-11-07 23:50:00 +0700

    check input

--- Chat-demo-master/Client/Client.cs
+++ Chat-demo-master/Client/Client.cs
@@ -11,12 +11,46 @@ namespace Client
 {
     public partial class Client : Form
     {
-       
+        private TcpClient _client;
+        private SslStream _sslStream;
 
         public Client()
         {
             InitializeComponent();
         }
 
-        
-}
+        //Kiểm tra dữ liệu nhập vào
+        private bool CheckInputs(out IPAddress ip, out int port, out string username)
+        {
+            ip = null;
+            port = 0;
+            username = string.Empty;
+
+            if (string.IsNullOrEmpty(txt_username.Text))
+            {
+                MessageBox.Show("Vui lòng nhập tên người dùng *_*", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txt_IPAddress.Text) || string.IsNullOrEmpty(txt_Port.Text))
+            {
+                MessageBox.Show("Vui lòng nhập địa chỉ IP và Port lớn hơn 1000 trở lên !!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (!IPAddress.TryParse(txt_IPAddress.Text, out ip))
+            {
+                MessageBox.Show("Vui lòng nhập đúng địa chỉ IP!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (!int.TryParse(txt_Port.Text, out port) || port <= 1000)
+            {
+                MessageBox.Show("Vui lòng nhập Port có giá trị lớn hơn 1000!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            username = txt_username.Text;
+            return true;
+        }
+    }
--- Chat-demo-master/Server/obj/Debug/net8.0-windows/Server.AssemblyInfo.cs
+++ Chat-demo-master/Server/obj/Debug/net8.0-windows/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9683d40a263314f7847fa8580be5702898c8e7a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
